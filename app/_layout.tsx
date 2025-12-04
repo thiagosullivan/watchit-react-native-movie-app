2bb4f85e319a224b018e46486f8e1344d9c298a9
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-04 00:21:35 -0300

    feat: change app title and icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="movie" options={{ headerShown: false }} />
+      <Stack.Screen name="movie/[id]" options={{ headerShown: false }} />
     </Stack>
   );
 }
