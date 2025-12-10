75439707df3b921c4af4aaf2c1c85df65c5bb956
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-09 22:09:22 -0300

    feat: search screen fetching and rendering movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -50,6 +50,8 @@ export default function Index() {
             <SearchBar
               onPress={() => router.push("/search")}
               placeholder="Search for a movie"
+              value=""
+              onChangeText={() => {}}
             />
             <>
               <Text className="text-lg text-white font-bold mt-5 mb-3">
