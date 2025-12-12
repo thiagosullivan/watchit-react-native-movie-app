57438fa93f4ced9541e6f6c81ec422c24477a9c0
thiagosullivan <thiagosullivan12@gmail.com>
2025-12-12 11:27:52 -0300

    feat: movie page details

--- components/TrendingCard.tsx
+++ components/TrendingCard.tsx
@@ -17,7 +17,7 @@ const TrendingCard = ({
           resizeMode="cover"
         />
 
-        <View className="absolute bottom-6 -left-2.5 px-2 py-1 rounded-full">
+        <View className="absolute bottom-9 -left-1.5 px-2 py-1 rounded-full">
           <MaskedView
             maskElement={
               <Text className="font-bold text-white text-6xl">{index + 1}</Text>
--- services/api.ts
+++ services/api.ts
@@ -26,3 +26,26 @@ export const fetchMovies = async ({ query }: { query: string }) => {
 
   return data.results;
 };
+
+export const fetchMovieDetails = async (
+  movieId: string
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      }
+    );
+
+    if (!response.ok) throw new Error("Failed to fetch movie details");
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
